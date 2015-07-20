590f053fe8f0e2347b06c95d01f20a73bc4cb1e1
Soumendra Daas <daas.soumendra@gmail.com>
2015-07-20 13:30:53 +0530

    Completed ex6 part 2

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,49 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+tol = 1e-3;         % input to svmTrain
+max_passes = 5;     % input to svmTrain
 
+Clist = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]';
+sigmaList = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30 ]';
 
+%Clist = [30, 10, 3, 1, 0.3, 0.1, 0.03, 0.01 ];
+%sigmaList = [30, 10, 3, 1, 0.3, 0.1, 0.03, 0.01 ];
 
+emin = length(yval)*(10);
+E = zeros(length(Clist),length(sigmaList));  % For debugging
 
+for i=1:length(Clist)
+    c = Clist(i);
+    for j=1:length(sigmaList)
+        s = sigmaList(j);
 
+        % create a model
+        model= svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s), tol, max_passes); 
+
+        % and use it to predict against the cross-validation set
+        predictions =  svmPredict(model, Xval);
+
+        % and compute the prediction error
+        e = mean(double(predictions ~= yval));
+
+        % for debugging
+        E(i,j) = e;
+
+        % Check if this is a minimum
+        if ( e < emin )
+            C = c;
+            sigma = s;
+            emin = e;
+        endif
+
+        fprintf('Current C = %f sigma = %f e = %f \n', c,s,e);
+        fprintf('   Best C = %f sigma = %f e = %f \n', C,sigma,emin);
+
+    end
+end
+
+E
 
 % =========================================================================
 
