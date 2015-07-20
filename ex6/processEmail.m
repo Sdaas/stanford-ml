bbd386b006c9a2deff6516d11a98a81cc76bc6df
Soumendra Daas <daas.soumendra@gmail.com>
2015-07-20 15:01:12 +0530

    completed ex6 part 3

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,14 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    for vi=1:length(vocabList)
+        if( strcmp(str, vocabList(vi)) == 1)
+            word_indices = [word_indices ; vi];
+            break;
+        end
+    end;
 
     % =============================================================
 
